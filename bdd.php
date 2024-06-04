fe1dd67d930d9563d024b37c75e7f886676d1b65
carmagnolleandrean <carmagnolleandrean@gmail.com>
2024-06-04 02:45:19 +0200

    Delete bdd.php

--- bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    session_start();
-    $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8;', 'root', '');
-}catch(Exception $e){
-    die('Une erreur est survenue :' . $e->getMessage());
-}
